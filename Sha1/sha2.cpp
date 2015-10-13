c2aa2967f5cae08422ab08466f5a5b45bacfaaca
Martin Engen <martie12@student.uia.no>
2015-10-13 11:18:18 +0200

    gitignore fix

---
+++ Sha1/sha2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mello on 06.10.2015.
+//
+
+#include "sha2.h"
---
+++ Sha1/sha2.h
@@ -0,0 +1,14 @@
+//
+// Created by Mello on 06.10.2015.
+//
+
+#ifndef SHA1_SHA2_H
+#define SHA1_SHA2_H
+
+
+class sha2 {
+
+};
+
+
+#endif //SHA1_SHA2_H
