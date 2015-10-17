4e1059a9a3681547374dfe26a33a49bcf463f181
Martin Engen <martie12@student.uia.no>
2015-10-17 18:09:09 +0200

    Sha1Proto alpha version done

--- Sha1Proto/main.cpp
+++ Sha1Proto/main.cpp
@@ -1,33 +1,28 @@
 #include <iostream>
 #include <bitset>
 #include <vector>
+#include <c++/sstream>
 
 using namespace std;
 
 
-unsigned int Ch(unsigned int x,unsigned int y,unsigned int z){
-    unsigned int f = x & y;
-    unsigned int e = ~x & z;
-
-    return f^e;
+uint32_t Ch(uint32_t x,uint32_t y,uint32_t z){
+    //uint32_t f = x & y;
+    //uint32_t e = ~x & z;
+    //return f^e;
+    return ( x & y) ^ (~x & z);
 }
 
-unsigned int Parity(unsigned int x,unsigned int y,unsigned int z){
+uint32_t Parity(uint32_t x,uint32_t y,uint32_t z){
     return x ^ y ^ z;
 }
 
-unsigned int Maj(unsigned int x,unsigned int y,unsigned int z){
+uint32_t Maj(uint32_t x,uint32_t y,uint32_t z){
     return (x & y) ^ (x & z) ^ (y & z);
 }
 
 
 int main() {
-    unsigned int tass = Ch(1231,240,123541);
-    std::bitset<32>x(tass);
-    cout << x;
-
-    //vector<bitset<8>> bitMessage;
-
     vector<uint8_t> bitsMessage;
 
     string input;
@@ -41,9 +36,6 @@ int main() {
 
 
     cout << endl;
-    //bitsMessage.push_back(0);
-    //bitsMessage.push_back(0);
-    //bitsMessage.push_back(128); // 1000 0000
     for(auto &attack : bitsMessage){
         cout << bitset<8>(attack) << " ";
     }
@@ -98,38 +90,17 @@ int main() {
     bitsMessage.push_back(t8);
 
     //Adde de siste 64 bits (8 byte)
-    /*
-    for(int i = 1;i<=8;i++){
-        bitsMessage.push_back((uint8_t)(t>>(64-(8*i))));
-        cout <<endl<< 64-(8*i) << endl << bitset<8>((uint8_t)(t));
-    }
-     */
 
     cout << endl;
-    //cout << bitsMessage.size();
+
     for(auto &attack : bitsMessage){
         cout << bitset<8>(attack) << " ";
     }
     cout <<endl<< "----------"<<endl;
     cout << bitsMessage.size();
     cout << endl;
-    //cout << (int)bitMessage[0].to_ulong() & (int)bitMessage[1].to_ulong() ;
-    //cout << bitsMessage[0] & bitsMessage[1];
-    uint32_t K1 = 0x5a827999;
-    uint32_t K2 = 0x6ed9eba1;
-    uint32_t K3 = 0x8f1bbcdc;
-    unsigned int K4 = 0xca62c1d6;
-
-    cout << K1 + K4;
-
-    uint32_t test = 2;
-    unsigned int e3 = bitsMessage[0] & bitsMessage[1];
-
 
     vector<uint32_t> Words;
-    int counter = 0;
-    //Words.push_back(bitsMessage[0] << 24 | bitsMessage[1]<<16 | bitsMessage[2]<<8 | bitsMessage[3]);
-    //cout << Words[0];
 
     for(int i = 0;i<bitsMessage.size();i = i + 4){
         Words.push_back(bitsMessage[i] << 24 | bitsMessage[i+1]<<16 | bitsMessage[i+2]<<8 | bitsMessage[i+3]);
@@ -143,9 +114,14 @@ int main() {
         cout << bitset<32>(current) << " ";
     }
 
-
+    //Constants
+    uint32_t K1 = 0x5a827999;
+    uint32_t K2 = 0x6ed9eba1;
+    uint32_t K3 = 0x8f1bbcdc;
+    uint32_t K4 = 0xca62c1d6;
 
     uint32_t digest[5];
+
     //Hex
     digest[0] = 0x67452301;
     digest[1] = 0xefcdab89;
@@ -159,49 +135,72 @@ int main() {
     uint32_t C = digest[2];
     uint32_t D = digest[3];
     uint32_t E = digest[4];
+    uint32_t T;
 
-    for(int t = 0;t<80;t++){
-        if(t < 20){
-            //Round 1
-            //cout << endl << t << "R1";
+    uint32_t Word;
 
+
+    //Beginning of block
+
+    for(int i = 1;i<2;i++){
+        for(int t = 0;t<80;t++){
+            if(t < 16){
+                Word = Words[t];
+                //cout << endl << t << " " << Word;
+            }
+            else{
+                Words.push_back(((Words[t-3] ^ Words[t-8] ^ Words[t-14] ^ Words[t-16]) <<1) | ((Words[t-3] ^ Words[t-8] ^ Words[t-14] ^ Words[t-16]) >> 32-1));
+                Word = Words[t];
+            }
+
+            if(t <= 19){            //Round 1
+                T = ((A << 5) | (A >> 32-5)) + Ch(B,C,D) + E + K1 + Word;
+            }
+            else if(t > 19 && t <= 39){
+                T = ((A << 5) | (A >> 32-5)) + Parity(B,C,D) + E + K2 + Word;
+            }
+            else if(t > 39 && t <= 59){            //Round 3
+                T = ((A << 5) | (A >> 32-5)) + Maj(B,C,D) + E + K3 + Word;
+            }
+            else{            //Round 4
+                T = ((A << 5) | (A >> 32-5)) + Parity(B,C,D) + E + K4 + Word;
+            }
+
+            E = D;
+            D = C;
+            C = ((B << 30) | (B >> 32-30));
+            B = A;
+            A = T;
 
         }
-        else if(t > 19 && t < 40){
-            //Round 2
-            //cout << endl << t <8< "R2";78
 
-        }
-        else if(t > 39 && t < 60){
-            //Round 3
-            //cout << endl << t << "R3";
-
-        }
-        else{
-            //Round 4
-            //cout << endl << t << "R4";
-
-
-        }
-
-
+        digest[0] = A + digest[0];
+        digest[1] = B + digest[1];
+        digest[2] = C + digest[2];
+        digest[3] = D + digest[3];
+        digest[4] = E + digest[4];
 
     }
 
+    //End of Block
 
-    /*
-    char a = 'a';
-    char c = 'c';
-    unsigned int b = a;
-    unsigned int d = c;
-    unsigned int e2 = b&d;
-    unsigned int e3 = bitsMessage[0] & bitsMessage[1];
-    cout << "---------------------" << endl << e3 << endl;
+    cout << "Message Digest is: "<<endl;
+    //cout <<"%X\n" << digest[0] << " %X\n" <<digest[1] << " %X\n" <<digest[2] << " %X\n" <<digest[3] << " %X\n" <<digest[4];
+    printf("%X",digest[0]);
+    printf(" %X",digest[1]);
+    printf(" %X",digest[2]);
+    printf(" %X",digest[3]);
+    printf(" %X",digest[4]);
 
-    cout << endl << K1 << endl;
+    //string answer = "%X" + digest[0];
 
-    cout << 512 - (1472%512);
-    */
+    std::stringstream sstream;
+    sstream << std::hex << digest[0] << digest[1] << digest[2] << digest[3]<< digest[4];
+    string answer = sstream.str();
+    cout <<endl;
+    cout << answer;
+
+
 
     return 0;
 }
